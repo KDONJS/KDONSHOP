9e5f65f81a96c487d7887d858ffa80d53d5096d4
KDONJS <80475432+KDONJS@users.noreply.github.com>
2021-07-31 13:25:29 -0500

    Update bd.php

--- administrador/config/bd.php
+++ administrador/config/bd.php
@@ -1,9 +1,9 @@
 <?php
 
-$host="localhost";
+$host="sitioo.mysql.database.azure.com";
 $bd="sitio";
-$usuario="root";
-$contraseña="";
+$usuario="Sitio@sitioo";
+$contraseña="Tato2407@";
 
 try {
     $conexion=new PDO("mysql:host=$host;dbname=$bd",$usuario,$contraseña);
